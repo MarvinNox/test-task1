4f22e8f3375a33a6b3ad6869f8d6ad8b4a28c694
Denys Tkachenko <sinedtex@gmail.com>
2025-07-28 14:33:21 +0300

    fix header styles

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -4,6 +4,9 @@ import css from "./Header.module.css";
 export default function Header() {
   return (
     <header className={css.header}>
+      <Link href="/" className={css.headerLogo}>
+        MiniBlog
+      </Link>
       <nav aria-label="Main Navigation" className={css.headerList}>
         <Link href="/" className={css.headerLink} aria-label="Home">
           Home
